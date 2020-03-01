85cc1224537e826403ed40e558ac849f8491d5be
ZoDSeR SyLVaN <zodser97@gmail.com>
2020-03-01 18:24:08 -0500

    Final pawn move

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -17,17 +17,17 @@ class Pawn(piece):
             if self.fMove is True:
                 self.piecesMoves.append([self.x_coord-2, self.y_coord])
                 self.fMove = False
-            if self.y_coord+1 < 7 and self.board[self.x_coord-1][self.y_coord+1].pieceOccupy.toString() != "0":
+            if self.y_coord+1 <= 7 and self.board[self.x_coord-1][self.y_coord+1].pieceOccupy.toString() != "0":
                 self.piecesMoves.append([self.x_coord-1, self.y_coord+1])
-            if self.y_coord-1 > 0 and self.board[self.x_coord-1][self.y_coord-1].pieceOccupy.toString() != "0":
+            if self.y_coord-1 >= 0 and self.board[self.x_coord-1][self.y_coord-1].pieceOccupy.toString() != "0":
                 self.piecesMoves.append([self.x_coord-1, self.y_coord-1])
         else:
             self.piecesMoves.append([self.x_coord+1, self.y_coord])
             if self.fMove is True:
                 self.piecesMoves.append([self.x_coord+2, self.y_coord])
                 self.fMove = False
-            if self.y_coord+1 < 7 and self.board[self.x_coord+1][self.y_coord+1].pieceOccupy.toString() != "0":
+            if self.y_coord+1 <= 7 and self.board[self.x_coord+1][self.y_coord+1].pieceOccupy.toString() != "0":
                 self.piecesMoves.append([self.x_coord+1, self.y_coord+1])
-            if self.y_coord-1 > 0 and self.board[self.x_coord+1][self.y_coord-1].pieceOccupy.toString() != "0":
+            if self.y_coord-1 >= 0 and self.board[self.x_coord+1][self.y_coord-1].pieceOccupy.toString() != "0":
                 self.piecesMoves.append([self.x_coord+1, self.y_coord-1])
         return checkPieces(board, self.piecesMoves, self)
