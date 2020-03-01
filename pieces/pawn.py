f6c639317406a937d9b90638b10555272d424424
ZoDSeR SyLVaN <zodser97@gmail.com>
2020-03-01 13:05:42 -0500

    fix minor detail

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -19,7 +19,7 @@ class Pawn(piece):
                 self.fMove = False
             if self.y_coord+1 < 7 and self.board[self.x_coord-1][self.y_coord+1].pieceOccupy.toString() != "0":
                 self.piecesMoves.append([self.x_coord-1, self.y_coord+1])
-            if self.y_coord+1 > 0 and self.board[self.x_coord-1][self.y_coord-1].pieceOccupy.toString() != "0":
+            if self.y_coord-1 > 0 and self.board[self.x_coord-1][self.y_coord-1].pieceOccupy.toString() != "0":
                 self.piecesMoves.append([self.x_coord-1, self.y_coord-1])
         else:
             self.piecesMoves.append([self.x_coord+1, self.y_coord])
