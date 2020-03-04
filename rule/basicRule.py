941642d23d9670652e0b06f65059e8978d327934
ZoDSeR SyLVaN <zodser97@gmail.com>
2020-03-04 10:52:51 -0500

    final checkPieces

--- rule/basicRule.py
+++ rule/basicRule.py
@@ -14,20 +14,18 @@ class checkPieces(Rule):
 
     def Check(self):
         for i in self.moveList:
-            print(i)
             if self.board[i[0]][i[1]].pieceOccupy.toString() != "0":
                 if self.board[i[0]][i[1]].pieceOccupy.alliance == self.piece.alliance:
                     self.moveList.remove([i[0], i[1]])
                 if self.piece.toString() != "N" or self.piece.toString() != "K":
                     for j in self.moveList:
-                        if j[1] < i[1] or j[0] < i[0]:
-                            self.moveList.remove([j[0], j[1]])
-                        elif j[1] > i[1] or j[0] > i[0]:
-                            self.moveList.remove([j[0], j[1]])
+                        if j[1] < i[1] < self.piece.y_coord or j[1] > i[1] > self.piece.y_coord:
+                            self.moveList.remove(j)
+                        elif (j[0] < i[0] < self.piece.x_coord or j[0] > i[0] > self.piece.x_coord) and j[1] == i[1] == self.piece.y_coord:
+                            self.moveList.remove(j)
                 if self.piece.toString() == "P":
-                    if self.piece.alliance == "W" and i[0] < self.piece.x_coord:
-                        self.moveList.remove([i[0], i[1]])
-                    elif self.piece.alliance == "W" and i[0] > self.piece.x_coord:
-                        self.moveList.remove([i[0], i[1]])
-
+                    if self.piece.alliance == "W" and i[0] < self.piece.x_coord and i[1] == self.piece.y_coord:
+                        self.moveList.remove(i)
+                    elif self.piece.alliance == "B" and i[0] > self.piece.x_coord and i[1] == self.piece.y_coord:
+                        self.moveList.remove(i)
         return self.moveList
