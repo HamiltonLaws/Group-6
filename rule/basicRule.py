7be8d7713803eab0ce1a46781367be15ac66abe6
Hamilton Laws <31488056+HamiltonLaws@users.noreply.github.com>
2020-03-26 13:45:46 -0400

    Update basicRule.py
    Added the check rule still needs to fix changing move placements and the protector rule

--- rule/basicRule.py
+++ rule/basicRule.py
@@ -50,6 +50,47 @@ class checkPieces(Rule):
                         elif j[1] < i[1] < self.piece.y_coord and j[0] > i[0] > self.piece.x_coord:   
                             self.removeList.append(j)
         self.moveList = [i for i in self.moveList if i not in self.removeList]
+        
+class checked:
+    pieceMoves = []
+    board = None
+    alliance= None
+    message = None
+    def __init__(self, board, alliance):
+        self.board = board
+        self.alliance = alliance
+
+    def Clear(self):
+        self.pieceMoves.clear()
+
+class Check(checked):
+    def _init_(self,board,alliance):
+        super().__init__(board,alliance)
+    
+    def isCheck(self):
+        super().Clear()
+        kingPlace = []
+        curent = None
+        curentMove = None
+        # pieceMoves retuns a list of all the posible moves from the current player, kingPlace returns the placment of the oppoite king
+        for rows in range(8):
+            for cols in range(8):
+                curent = self.board[rows][cols].pieceOccupy #need to acount for moving own pices could make check
+                if(curent.toString() != "0" and curent.toString() != "K" and curent.alliance == self.alliance):
+                    curentMove = curent.validMove(self.board)
+                    self.pieceMoves.append(curentMove)
+                elif(curent.toString() == "K" and curent.alliance != self.alliance):
+                    kingPlace = [curent.x_coord,curent.y_coord]
+        #check the other king to see if your             
+        for i in range(len(self.pieceMoves)):
+            for j in range(len(self.pieceMoves[i])):
+                print(self.pieceMoves[i][j])
+                if(kingPlace == self.pieceMoves[i][j]):
+                    self.message = "Is In Check"
+
+            #maby make the check condition a boolian
+        return self.message       
+
 
 class enPassant(Rule):
     # Precondition for the attack square to be added
