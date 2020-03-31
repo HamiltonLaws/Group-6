7f9114f4d5049b3b4ac6eae6913eaf185f602f7a
ZoDSeR SyLVaN <zodser97@gmail.com>
2020-03-31 14:45:43 -0400

    remove bot file until resolve arrangement

--- Bot.py
+++
@@ -1,9 +0,0 @@
-import random
-from board.chessBoard import Board
-from pieces.nullPiece import nullPiece
-from chessMain import promoteCheck
-
-class Bot:
-    board = None
-    minionList = list()
-    def __init__(self, board, pieces):
